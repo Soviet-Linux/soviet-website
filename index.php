836b599d681b523dcd9d6c139e2167349bad479b
tbeckerson <trevorbeckerson@gmail.com>
2022-04-13 13:32:13 -0300

    switch to grid layout for header

--- index.php
+++ index.php
@@ -28,58 +28,52 @@ Do not change anything unless you know what you're doing!
   -->
   <style>
   </style>
-<?php
-  include 'php/php_scripts.php';
-  include 'php/Parsedown.php';
-?>
+  <?php
+    include 'php/php_scripts.php';
+    include 'php/Parsedown.php';
+  ?>
 </head>
 
 <!--
 Below here is the visible part of the website. Any classes added to the body tag will be applied to the whole site, unless they're overridden in the tags below.
 We're using the Hind font as the default for text. The php is calling the background image that shows through the rays -->
-<body class="font-hind bg-top contain" style="background-image: url('<?php echo $bg_image_1; ?>');">
+<body class="font-hind bg-center bg-top-l bg-size-75" style="background-image: url('<?php echo $bg_image_1; ?>');">
 
 <!-- the header is the logo, the penguin, and the link to the iso. the entire first screen. -->
-<header class="vh-100 flex flex-column align-center justify-start cover bg-leftbottom" style="background-image: url('assets/rays.svg');">
+<header class="vh-100-l grid grid-3-column grid-row-50 bg-leftbottom ani-fade-in ani-1s" style="background-image: url('assets/rays.svg');">
 
-  <section class="vh-50">
-  <!-- the big SOVIET LINUX title, and the subtitle -->
-    <div class="font-russian f1 f-subheadline-m f-headline-l tracked white tc ani-fade-in ani-1s pt3 pt4-m pt6-l">SOVIET&#9773; LINUX<br />
-  <!-- the subheadline is nested into the title's div to keep them together on the screen -->
-      <div class="font-teko b f4 f3-ns tracked white">The source based distribution for the people
+<!-- the big SOVIET LINUX title, and the subtitle -->
+    <div class="self-center gc-start-1 gc-end-4 font-russian f1 f-subheadline-m f-headline-l tracked white tc ani-fade-in ani-2s ani-delay-1s ani-backwards">SOVIET&#9773; LINUX<br />
+<!-- the subheadline is nested into the title's div to keep them together on the screen -->
+      <div class="font-teko b f3 f3-ns tracked white ani-fade-in ani-1s ani-delay-15s ani-backwards">The source based distribution for the people
       </div>
     </div>
-  </section>
 
-  <!-- the SECTION contains the Tux image that slides in, and the iso download link -->
-  <section class="vh-50 w-100 flex flex-row align-center justify-center pt2">
-  <!-- there's two <div>s within the section, this one is the Tux graphic -->
-  
-  <!-- the penguin image -->
-      <div class="self-end absolute left-0 z-0">
-        <img class="vh-50 v-btm ani-fade-in-right ani-2s ani-ease-out" src="assets/tux-soviet.svg" alt="image of an angry penguin" />
-      </div>
-  
-  <!-- this <div> contains the iso file and the above & below stars -->
-      <div class="tc self-start z-1">
-        <!-- top star -->
-        <p class="f3 tc white">&#9733;</p>
-  <!-- the href in this <a> section below is the actual link to the iso -->
-        <a class="no-underline ph2 bg-dark-red hover-bg-sov-red yellow hover-light-yellow font-teko b f3 f2-s f1-l f3 tc bg-animate ba bw1 bw2-ns b--dark-gray br3 ani-fade-in ani-1s ani-ease-out"
-        href="https://drive.google.com/file/d/1-jQcLdOwEA-Fzbn1r6LPaY4E5O0TGIwj/view?usp=sharing"
-        >
-  <!-- I'm calling it soviet-current.iso, but if there's a better name go ahead and change it -->
-          SOVIET-CURRENT.ISO
-        </a>
-        <!-- bottom star -->
-        <p class="f3 tc white">&#9733;</p>
-      </div>
-  </section> 
+<!-- the iso file and the side stars -->
+    <section class="tc z-1 gr-start-2 gc-start-1 gc-end-4 ani-fade-in ani-1s ani-delay-2s ani-backwards">
+      <!-- left star -->
+      <div class="dib f1 tc white pr2">&#9733;</div>
+<!-- the href in this <a> section below is the actual link to the iso -->
+      <a class="no-underline ph3 pv3 bg-dark-red hover-bg-light-yellow yellow hover-sov-red font-teko b f3 f2-s f1-l f3 tc bg-animate ba bw1 bw2-ns b--dark-gray br3"
+      href="https://drive.google.com/file/d/1-jQcLdOwEA-Fzbn1r6LPaY4E5O0TGIwj/view?usp=sharing"
+      >
+<!-- I'm calling it soviet-current.iso, but if there's a better name go ahead and change it -->
+        SOVIET-CURRENT.ISO
+      </a>
+      <!-- bottom star -->
+      <div class="dib f1 tc white pl2">&#9733;</div>
+
+    </section>
+<!-- the penguin image -->
+    <section class="self-end gr-start-2 gc-start-1 gc-end-4">
+      <img class="mw-100 mvh-50 w-80-m w-75-l v-btm ani-fade-in-right ani-2s ani-ease-out" src="assets/tux-soviet.svg" alt="image of an angry penguin" />
+    </section>
+     
 </header>
 
 <!-- the main content of the page - more stuff will come in here eventually.
  Right now, <main> is acting as a container for the content. It's got the fixed position background image for the section, and the php calls an image from the randomizer -->
-<main class="bg-sov-red bg-top contain bg-fixed" style="background-image: url('<?php echo $bg_image_2; ?>');">
+<main class="bg-sov-red bg-center bg-top-l contain bg-fixed" style="background-image: url('<?php echo $bg_image_2; ?>');">
   <!-- this section is the about info. It's got a semi-transparent background, which lets the image from <main> show through -->
   <?php
   $file = "./markdown/about.md";
@@ -103,7 +97,7 @@ We're using the Hind font as the default for text. The php is calling the backgr
 </main>
 
 <!-- the <footer> is doing the same as <main> right now - it's a container with a background image. The php calls an image from the randomizer -->
-<footer class="bg-dark-gray bg-center bg-size-50" style="background-image: url('<?php echo $bg_image_3; ?>')">
+<footer class="bg-dark-gray bg-center bg-size-75 bg-50-m" style="background-image: url('<?php echo $bg_image_3; ?>')">
 
 <!-- this section is semi-transparent grey, which lets the <footer> background image show through. It's using flexbox instead of paragraphs so that we can add more links in later and readjust the flow (if needed)-->
   <section class="pv3 ph2 bg-dark-gray-t9 flex flex-column items-center justify-center">
