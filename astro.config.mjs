306f35b447cd8dca7f660901c214da9c97e3065f
Jeff Bezos <me@theholytachanka.com>
2024-10-19 11:13:02 +0000

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), tailwind()]
+});
